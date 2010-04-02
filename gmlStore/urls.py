e5447deeebd7a1b4bc4d926a3cdb856656fb9f16
Andrew Shirley <andrew@als-bi.com>
2010-04-02 11:26:45 +0000

    first crack at a template and a 'store' view to be used by AJAX.

--- gmlStore/urls.py
+++ gmlStore/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls.defaults import *
 
 urlpatterns = patterns('mapbin.gmlStore.views',
     (r'^$', 'index'),
+    (r'^store$', 'store'),
     (r'^(?P<gml_id>\d+)$', 'info'),
     (r'^(?P<gml_id>\d+)/get$', 'get'),
     (r'^(?P<gml_id>\d+)/info$', 'info'),
--- gmlStore/views.py
+++ gmlStore/views.py
@@ -1,16 +1,27 @@
 # Create your views here.
 from mapbin.gmlStore.models import Annotation
 from django.http import HttpResponse
-from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404, render_to_response
+import datetime
 
+#normal web 1.0 views
 
 def index(request):
     return HttpResponse("You're looking at gml index.")
 
+def info(request, gml_id):
+    return render_to_response('gmlStore/info.html', {'gml_id': gml_id})
+
+
+# AJAX views
+
 def get(request, gml_id):
     annotation = get_object_or_404(Annotation, pk=gml_id)
-    output = annotation.gml
-    return HttpResponse(output)
+    return HttpResponse(annotation.gml, mimetype="text/xml")
 
-def info(request, gml_id):
-    return HttpResponse("You're looking at gml %s." % gml_id)
+def store(request):
+    #TODO: fail gracefully if this isn't a post with the right fields.
+    annotation = Annotation(gml=request.POST['gml'], creation_date=datetime.now())
+    annotation.save()
+    return HttpResponse(("<stored id='%s' />" % (annotation.id)), mimetype="text/xml")
+
